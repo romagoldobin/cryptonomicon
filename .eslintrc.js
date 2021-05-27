06682497ba15f69a09122dd0c2d93e82b5597781
Roman Goldobin <rgoldobin@appliedtech.ru>
2021-05-27 23:55:06 +0500

    start markup

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
   ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-vars': 'error',
   },
 };
